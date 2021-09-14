6964b854beb841646b3b195cc4b70f3fc5c2da72
nikiforniket <nikeet@exiverprojects.com>
2021-09-15 01:22:29 +0530

    Searching:: Linear and binary search.

---
+++ searching/binary_search.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+test_cases = [
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
+    {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
+    {'input': {'cards': [3, -1, -9, -127], 'query': -127}, 'output': 3},
+    {'input': {'cards': [6], 'query': 6}, 'output': 0},
+    {'input': {'cards': [9, 7, 5, 2, -9], 'query': 4}, 'output': -1},
+    {'input': {'cards': [], 'query': 7}, 'output': -1},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 3}, 'output': 7},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],'query': 6},'output': 2}
+]
+
+def check_function(cards, check, query):
+    if cards[check] == query:
+        if check-1 >=0  and cards[check -1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif cards[check] < query:
+        return 'left'
+    else:
+        return 'right'
+        
+def binary_search(cards, query):
+    lo, hi = 0, len(cards) - 1
+    check = None
+    while lo <= hi:
+        check = (lo+hi)//2
+        check_value = check_function(cards, check, query) 
+        if check_value == 'found':
+            return check
+        elif check_value == 'left':
+            hi = check - 1
+        elif check_value == 'right':
+            lo = check + 1
+    return -1
+
+
+for test_case in test_cases:
+    result = binary_search(**test_case['input'])
+    if result == test_case['output']:
+        print(True, result)
+    else:
+        print(False, result)
---
+++ searching/linear_search.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+test_cases = [
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
+    {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
+    {'input': {'cards': [3, -1, -9, -127], 'query': -127}, 'output': 3},
+    {'input': {'cards': [6], 'query': 6}, 'output': 0},
+    {'input': {'cards': [9, 7, 5, 2, -9], 'query': 4}, 'output': -1},
+    {'input': {'cards': [], 'query': 7}, 'output': -1},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 3}, 'output': 7},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],'query': 6},'output': 2}
+]
+
+def locate_cards(cards, query):
+    position = 0
+    print(len(cards))
+    while position < len(cards):
+        if cards[position] == query:
+            return position
+        position += 1
+    return -1
+
+for test_case in test_cases:
+    result = locate_cards(**test_case['input'])
+    if result == test_case['output']:
+        print(True, result)
